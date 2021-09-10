99dce7cc351b8432ffdfe476551f6223d473c590
divyanshprakash <shahparvshah@gmail.com>
2021-09-10 09:50:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
-ADMIN = os.getenv("ADMIN")
-DB_URL = os.getenv("DB_URL")
-DB_NAME = os.getenv("DB_NAME")
+API_ID = int(os.getenv("API_ID", "7595525"))
+API_HASH = os.getenv("API_HASH", "578d03250ace0f2f5ca53495a77cb18d")
+BOT_TOKEN = os.getenv("1938493507:AAEJo0KdQSEDdfPTSMm_nUKPxeG2AK6TIWw")
+SESSION_NAME = os.getenv("AQC_8Ne0bmh8TQcz-lNOt2EgT34_X781AvrN2QKqksi2qCschvHpqwxTd5u2JOpZcBHoKHs664CauhCgcNlYpNH0skFaUnFUY10Pte8perqVUWVHNB0CD_GmxImw55LxD-N57i-Kl8i5q2nA0yMdUKHX-wuO5qAPSUQ7D1B_C_aapr2dCFa__8r1_eR3U0N7zrHSN-p5_XWok_rlESaDrVcFrnSoKS3k8MUTE_FmIx_dxwtN0axw4JiOZs4j87Sah9NfHec7bzsURWbq7w6LHFDQdth_Qo-08p0LXgP8Xf7JZzuxq5Uvj81PpFWLQHg3XfKKihh7wNVOqh4R0uN0roQUdeaAZwA")
+ADMIN = os.getenv("691224603")
+DB_URL = os.getenv("mongodb+srv://parvdb:parvdb@cluster0.g4yvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DB_NAME = os.getenv("parvd")
