a22f589f24ba5e0bf905d935060e910f766ffb11
divyanshprakash <shahparvshah@gmail.com>
2021-09-10 10:26:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "7595525"))
-API_HASH = os.getenv("API_HASH", "578d03250ace0f2f5ca53495a77cb18d")
-BOT_TOKEN = os.getenv("1938493507:AAEJo0KdQSEDdfPTSMm_nUKPxeG2AK6TIWw")
-SESSION_NAME = os.getenv("AQC_8Ne0bmh8TQcz-lNOt2EgT34_X781AvrN2QKqksi2qCschvHpqwxTd5u2JOpZcBHoKHs664CauhCgcNlYpNH0skFaUnFUY10Pte8perqVUWVHNB0CD_GmxImw55LxD-N57i-Kl8i5q2nA0yMdUKHX-wuO5qAPSUQ7D1B_C_aapr2dCFa__8r1_eR3U0N7zrHSN-p5_XWok_rlESaDrVcFrnSoKS3k8MUTE_FmIx_dxwtN0axw4JiOZs4j87Sah9NfHec7bzsURWbq7w6LHFDQdth_Qo-08p0LXgP8Xf7JZzuxq5Uvj81PpFWLQHg3XfKKihh7wNVOqh4R0uN0roQUdeaAZwA")
-ADMIN = os.getenv("691224603")
-DB_URL = os.getenv("mongodb+srv://parvdb:parvdb@cluster0.g4yvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DB_NAME = os.getenv("my")
+API_ID = "7595525"
+API_HASH = "578d03250ace0f2f5ca53495a77cb18d"
+BOT_TOKEN = "1938493507:AAEJo0KdQSEDdfPTSMm_nUKPxeG2AK6TIWw"
+SESSION_NAME = "AQC_8Ne0bmh8TQcz-lNOt2EgT34_X781AvrN2QKqksi2qCschvHpqwxTd5u2JOpZcBHoKHs664CauhCgcNlYpNH0skFaUnFUY10Pte8perqVUWVHNB0CD_GmxImw55LxD-N57i-Kl8i5q2nA0yMdUKHX-wuO5qAPSUQ7D1B_C_aapr2dCFa__8r1_eR3U0N7zrHSN-p5_XWok_rlESaDrVcFrnSoKS3k8MUTE_FmIx_dxwtN0axw4JiOZs4j87Sah9NfHec7bzsURWbq7w6LHFDQdth_Qo-08p0LXgP8Xf7JZzuxq5Uvj81PpFWLQHg3XfKKihh7wNVOqh4R0uN0roQUdeaAZwA"
+ADMIN = "691224603"
+DB_URL = "mongodb+srv://parvdb:parvdb@cluster0.g4yvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DB_NAME = "my"
